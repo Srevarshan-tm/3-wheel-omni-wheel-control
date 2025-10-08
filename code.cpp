dd02f174fb64a77b0fd0d49a00c3b1f95fc2e399
Srevarshan-tm <srevarshantm@gmail.com>
2025-10-08 10:35:49 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -24,8 +24,7 @@ const int M2_DIR = 5;
 const int M3_PWM = 23;
 const int M3_DIR = 21;
 //defining the constant 
-const int DeadZONE = 20;
-const int MAX_JOY  = 127;   
+const int DeadZONE = 20;  
 const int MAX_PWM  = 75;   
 
 
@@ -38,9 +37,13 @@ WheelSpeeds speed_set(float ax, float ay, float w) {
     return ws;
 }
 //maping the joystick value with the max _joy
-float mapJoystick(int val) {
-    if (abs(val) < DeadZONE) return 0;     
-    return (float)val / MAX_JOY;           
+float normalizeJoystick(int val) {
+    int center = 127;
+    int DeadZONE = 20;
+    int offset = val - center;
+
+    if (abs(offset) < DeadZONE) return 0;
+    return (float)offset / 128.0;  //converting the values in the range of -1 to 1
 }
 //getting the pwm val and also setting the direction of the  motor
 int Getting_PWM(float val) {
@@ -52,7 +55,7 @@ int Getting_PWM(float val) {
 void setMotor(int pwmPin, int dirPin, int val) {
     if (val >= 0) {
         digitalWrite(dirPin, HIGH);
-        analogWrite(pwmPin, val-);
+        analogWrite(pwmPin, val);
     } else {
         digitalWrite(dirPin, LOW);
         analogWrite(pwmPin, -val);
@@ -76,12 +79,17 @@ void setup() {
 
 void loop() {
     myusb.Task();
-
+    int joyX,joyY,joyRot=0;
+    if (joyX==0 && joyY==0 && joyRot==0){
+        setMotor(M1_PWM, M1_DIR, 0);
+        setMotor(M2_PWM, M2_DIR, 0);
+        setMotor(M3_PWM, M3_DIR, 0);
+    }
     if (joystick1.available()) {
         //getting the valu from the joy stick  
-        int joyX =joystick1.getAxis(0);   
-        int joyY =joystick1.getAxis(1); 
-        int joyRot =joystick1.getAxis(2); 
+        joyX =joystick1.getAxis(0);   
+        joyY =joystick1.getAxis(1); 
+        joyRot =joystick1.getAxis(2); 
     }
     // If joystick is near center → stop all motors
     if (abs(joyX) < 20 && abs(joyY) < 20 && abs(joyRot) < 20){
@@ -92,24 +100,23 @@ void loop() {
         return;
     }
         //sending the raw value from the joystick to the mapping function  
-        float ax = mapJoystick(joyX);
-        float ay = mapJoystick(-joyY); 
-        float w  = mapJoystick(joyRot);
-        WheelSpeeds ws = speed_set(ax, ay, w);
-        int pwm1 = Getting_PWM(ws.f1);
-        int pwm2 = Getting_PWM(ws.f2);
-        int pwm3 = Getting_PWM(ws.f3);
-        setMotor(M1_PWM, M1_DIR, pwm1);
-        setMotor(M2_PWM, M2_DIR, pwm2);
-        setMotor(M3_PWM, M3_DIR, pwm3);
-        //for testing so printing the values in the serial monitor 
-        Serial.print("f1: "); Serial.print(pwm1);
-        Serial.print("  f2: "); Serial.print(pwm2);
-        Serial.print("  f3: "); Serial.println(pwm3);
-        Serial.print("  :joyx "); Serial.println(joyX);
-        Serial.print("  :joyy "); Serial.println(joyY);
-        Serial.print("  :joyrot"); Serial.println(joyRot);
-        delay(2000);
-
-    }
+    float ax = normalizeJoystick(joyX);
+    float ay = -normalizeJoystick(joyY);
+    float w  = normalizeJoystick(joyRot);
+    WheelSpeeds ws = speed_set(ax, ay, w);
+    int pwm1 = Getting_PWM(ws.f1);
+    int pwm2 = Getting_PWM(ws.f2);
+    int pwm3 = Getting_PWM(ws.f3);
+    setMotor(M1_PWM, M1_DIR, pwm1);
+    setMotor(M2_PWM, M2_DIR, pwm2);
+    setMotor(M3_PWM, M3_DIR, pwm3);
+    //for testing so printing the values in the serial monitor 
+    Serial.print("f1: "); Serial.print(pwm1);
+    Serial.print("  f2: "); Serial.print(pwm2);
+    Serial.print("  f3: "); Serial.println(pwm3);
+    Serial.print("  :joyx "); Serial.println(joyX);
+    Serial.print("  :joyy "); Serial.println(joyY);
+    Serial.print("  :joyrot"); Serial.println(joyRot);
+    delay(2000);
 }
+
